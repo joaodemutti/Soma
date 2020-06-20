f2ebbf581af91d26ed7879caa904b01964d1468f
joaodemutti <joaofacindemutti@gmail.com>
2020-06-19 21:31:27 -0300

    CalculadoraPAM
    Exercicio de PAM.
    Criar e clonar um repositório no Github.
    Uma calculadora feita apartir do exemplo da professora, com a adição das possíveis operações: subtração, multiplicação e divisão.
    É avisado ao usúario de que jamais dividirás por zero.
    Utilização do Github

--- app/src/main/java/com/exercicos/aulas/soma/MainActivity.java
+++ app/src/main/java/com/exercicos/aulas/soma/MainActivity.java
@@ -8,10 +8,13 @@ Quando é necessário importar uma biblioteca a classe fica em vermelho por não
 */
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 /*
@@ -22,11 +25,12 @@ public class MainActivity extends AppCompatActivity {
 
     EditText num1;
     EditText num2;
+    Spinner spinOper;
     TextView result;
-    Button btnSoma;
+    Button btnCalc;
 
     //Variaveis locais para manipulação de Valores
-    int n1,n2,soma;
+    double n1,n2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,7 +46,8 @@ public class MainActivity extends AppCompatActivity {
         num1 = (EditText)findViewById(R.id.num1);
         num2 = (EditText)findViewById(R.id.num2);
         result = (TextView)findViewById(R.id.resultado);
-        btnSoma = (Button)findViewById(R.id.btnCalcula);
+        btnCalc = (Button)findViewById(R.id.btnCalcula);
+        spinOper = (Spinner)findViewById(R.id.spinOper);
 
     }
 
@@ -50,12 +55,40 @@ public class MainActivity extends AppCompatActivity {
     //Recebe sempre a classe View como parametrro, que contém as informações da origem da ação
     public void Calcula(View view){
         //recupera o valor do elemento
-        n1 = Integer.parseInt(num1.getText().toString());
-        n2 = Integer.parseInt(num2.getText().toString());
-        soma = n1 + n2;
+        String operacao = spinOper.getSelectedItem().toString();
+        String[] options= getResources().getStringArray(R.array.spinnerArray);
 
+        n1 = Double.parseDouble(num1.getText().toString());
+        n2 = Double.parseDouble(num2.getText().toString());
+        Log.println(Log.ASSERT,"soma",operacao + "==" + options[0]);
+        double resp = 06.06d;
+        if(operacao.equals(options[0]))
+        {
+            Log.println(Log.ASSERT,"soma","a");
+            resp = n1 + n2;
+        }
+        if(operacao.equals(options[1]))
+        {
+            resp = n1 - n2;
+        }
+        if(operacao.equals(options[2]))
+        {
+            resp = n1 * n2;
+        }
+        if(operacao.equals(options[3]))
+        {
+            if(n2 != 0)
+            {
+                resp = (n1 / n2);
+            }
+            else
+            {
+                Toast.makeText(this,getResources().getString(R.string.errorMessageIV),Toast.LENGTH_LONG).show();
+                return;
+            }
+        }
         //modifica o valor do elemento para exibir em tela
-        result.setText(Integer.toString(soma));
+        result.setText(Double.toString(resp));
     }
 
 
